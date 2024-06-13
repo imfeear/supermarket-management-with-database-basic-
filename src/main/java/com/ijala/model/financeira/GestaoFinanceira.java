744d60c339198833d46aac013d98648890217b69
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2024-06-13 11:30:04 -0300

    add Gestão Financeira

---
+++ src/main/java/com/ijala/model/financeira/GestaoFinanceira.java
@@ -0,0 +1,40 @@
+package com.ijala.model.financeira;
+
+public class GestaoFinanceira {
+    private String tipo;
+    private String categoria;
+    private String data;
+    private double quantidade;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+}
